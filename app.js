69a4a00954cd316ecb1d8d736029d8363b5f2c34
Dillon_Chanis <dillonchanis92@gmail.com>
2016-01-17 14:25:24 -0500

    Required the router.js file. Used the home function to display the home page.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //Require the router files
-
+var router = require("./router.js");
 
 //Create the web server
 var http = require("http");
 http.createServer(function(request, response){
+	router.home(request, response);
 	response.end("It works!");
 }).listen(8080);
 
