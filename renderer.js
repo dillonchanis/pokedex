b3ad0c87a24fd06e3c9214d80edbdddf81aae463
Dillon_Chanis <dillonchanis92@gmail.com>
2016-01-18 17:17:21 -0500

    Created new function mergeValues which takes the values and fileContents and replaces the placeholders with the values found in the obj

--- renderer.js
+++ renderer.js
@@ -1,15 +1,28 @@
 var fs = require("fs");
 
+function mergeValues(values, content) {
+	//Cycle over the keys of values object
+	for(var key in values) {
+		//Replace {} placeholders w/ values from values obj
+		content = content.toString().replace("{" + key + "}", values[key]);
+	}
+
+	return content;
+}
+
 function view(templateName, values, response){
 	//Read from template file
 	//Don't want async, order matters
 	var fileContents = fs.readFileSync('./views/' + templateName + '.html');
 
 	//Insert values to the content
+	fileContents = mergeValues(values, fileContents);
 
 	//Write out the contents to the response
 	response.write(fileContents);
 }
+
+
 
 //Export
 module.exports.view = view;
