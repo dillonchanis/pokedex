82c17ba2a35ac034905f5d882804f7d9c54fcc21
Dillon_Chanis <dillonchanis92@gmail.com>
2016-01-18 10:03:57 -0500

    Script to receive data stream chunks and parse it into a usable JSON object. Basic error handling

---
+++ profile.js
@@ -0,0 +1,54 @@
+var http = require("http");
+var EventEmitter = require("events").EventEmitter;
+
+//Function to get Pokemon Info
+function Profile(pokemon) {
+
+	EventEmitter.call(this);
+
+    profileEmitter = this;
+
+	//Connect to the API
+	var request = http.get("http://pokeapi.co/api/v1/pokemon/" + pokemon + "/", function(response){
+	
+		//Will hold the data that streams in
+		var body = "";
+
+		//Connection error
+		if(response.statusCode !== 200){
+			//abort the request
+			request.abort();
+			//Give error message + ({statuscode in here})
+			profileEmitter.emit("error", new Error("There was an error getting the information for" + pokemon + ". (" +
+				http.STATUS_CODES[response.statusCode] + ")"));
+		}//end of connection error
+
+		//Read the data chunks we recieve back in our 'body' var.
+		//This will be a string 
+		response.on('data' function(chunk){
+			body += chunk;
+			profileEmitter.emit("data", chunk);
+		});
+
+		//Use JSON.parse() to convert our body string
+		//To an object so we can access programatically
+		response.on('end', function(){
+			//if connection is good
+			//TRY to parse the data into JSON object
+			if(response.statusCode === 200){
+				try {
+					var profile = JSON.parse(body);
+					profileEmitter.emit("end", profile);
+				} catch (error) {
+					profileEmitter.emit("error", error);
+				}
+			}
+		}).on("error", function(error){
+			profileEmitter.emit("error", error);
+		});
+	});//end of request
+
+}//end of Profile
+
+//Export
+module.exports = Profile;
