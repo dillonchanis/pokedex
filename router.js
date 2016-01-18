0b414770053afa7cc23dcdfa08243fa187b4bd32
Dillon_Chanis <dillonchanis92@gmail.com>
2016-01-18 10:06:15 -0500

    Require profile.js to obtain pokemon info, also added a commonHeaders variable to keep code DRY

--- router.js
+++ router.js
@@ -1,10 +1,17 @@
+//Scripts to handle the webpage routing
+
+var Profile = require("./profile.js");
+var querystring = require("querystring");
+
+var commonHeaders = {'Content-Type': 'text/html'};
+
 //Handle HTTP route GET
 //This will be the Home Page
 function home(request, response) {
 	//if url == "/" && GET
 	if(request.url === "/"){
 		//show content
-		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.writeHead(200, commonHeaders);
 		response.write("Header\n");
 		response.write("Search\n");
 		response.end("Footer\n");
@@ -13,14 +20,14 @@ function home(request, response) {
 
 //User's profile page routing
 function user(request, response){
-	//if url === "/{username}"
-	//take away the / and extract the username
-	var username = request.url.replace("/", "");
-	//Make sure a username was entered
-	if(username.length > 0){
-		response.writeHead(200, {'Content-Type': 'text/plain'});
+	//if url === "/{pokemon}"
+	//take away the / and extract the pokemon name
+	var pokemon = request.url.replace("/", "");
+	//Make sure a pokemon name was entered
+	if(pokemon.length > 0){
+		response.writeHead(200, commonHeaders);
 		response.write("Header\n");
-		response.write(username + "\n");
+		response.write(pokemon + "\n");
 		response.end("Footer\n");
 	}
 }
